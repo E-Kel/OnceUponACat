f15e9de72a18853e68b6927fcd7ed7866375687f
QA engineer <kotarakh@gmail.com>
2020-03-18 00:28:15 +0200

    Story Done

--- src/OnceUponACat/HowDartWaderBecomeNice.java
+++ src/OnceUponACat/HowDartWaderBecomeNice.java
@@ -29,7 +29,7 @@ public class HowDartWaderBecomeNice {
                 "SithCat",
                 "Darth Wader",
                 palpatin,
-                "xoo-khhhh it's time to play!",
+                "xoo-khhh it's time to play!",
                 "{Spoiling the Master's lightsaber with force}",
                 "{is sitting on his little sith throne and xooooo-khhhhhhhh}");
 
